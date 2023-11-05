1b6b1633de6d142d72ab5feb5ee22cf816ae4de4
Adi <adityasrivastava807@gmail.com>
2023-11-05 13:33:41 +0530

    First Commit

---
+++ Assets/Scripts/urlButton.cs
@@ -0,0 +1,41 @@
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class urlButon : MonoBehaviour
+ {
+    public void hackcbs()
+    {
+        Application.OpenURL("https://hackcbs.tech/");
+    }
+    public void devfolio()
+    {
+        Application.OpenURL("https://hackcbs-6.devfolio.co//");
+    }
+    public void discord()
+    {
+        Application.OpenURL("https://discord.com/invite/ZeUgR2kwpA");
+    }
+    public void venue()
+    {
+        Application.OpenURL("https://maps.app.goo.gl/oFfbVLS96tSJEq4Q9");
+    }
+     public void clgLoc()
+    {
+        Application.OpenURL("https://www.google.com/maps/place/ABESIT+College+Of+Engineering/@28.6336112,77.4454967,17z/data=!3m1!4b1!4m5!3m4!1s0x390cee3d4e3485ed:0xe0fe1689b57c7d2e!8m2!3d28.6336112!4d77.4476854?coh=164777&entry=tt&shorturl=1");
+    }
+    public void clgSite()
+    {
+        Application.OpenURL("https://www.abesit.in/");
+    }
+    public void admissions()
+    {
+        Application.OpenURL("https://www.abesit.in/admission/");
+    }
+     public void courses()
+    {
+        Application.OpenURL("https://www.abesit.in/courses/");
+    }
+
+
+ }
+ 
